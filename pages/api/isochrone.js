5122a03d02d124e415ab0a555ac9270924d2666a
EdvinasJusis <edvinas.jusis@traveltime.com>
2023-01-10 13:09:03 +0200

    updates time map

--- pages/api/isochrone.js
+++ pages/api/isochrone.js
@@ -1,11 +1,12 @@
+import { TravelTimeClient } from 'traveltime-api';
 import { randomBytes } from 'crypto';
 import * as config from '../../config.json';
 import { validateGtm } from '../../lib/utils';
 
-const traveltimejs = require('traveltimejs');
-
-process.env.TRAVELTIME_ID = '';
-process.env.TRAVELTIME_KEY = '';
+const travelTimeClient = new TravelTimeClient({
+  apiKey: 'YOUR_APP_KEY',
+  applicationId: 'YOUR_APP_ID',
+});
 
 export default async function handler(req, res) {
   try {
@@ -14,7 +15,7 @@ export default async function handler(req, res) {
     if (config.gmtModifier && validateGtm(config.gmtModifier)) {
       departure = `${timestamp.substring(0, timestamp.lastIndexOf('.'))}${config.gmtModifier}:00`;
     }
-    const result = await traveltimejs.time_map({
+    const result = await travelTimeClient.timeMap({
       departure_searches: [
         {
           id: randomBytes(16).toString('hex'),
